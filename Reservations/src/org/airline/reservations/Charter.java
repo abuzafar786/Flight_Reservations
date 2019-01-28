44c76f70e02ef1e08a65ffb258fdd10d5315e481
german <germanjongewaard@gmail.com>
2019-01-28 13:43:37 +0100

    adding a Charter class

---
+++ Reservations/src/org/airline/reservations/Charter.java
@@ -0,0 +1,30 @@
+package org.airline.reservations;
+
+import java.time.LocalDateTime;
+
+public class Charter extends Flight{
+	
+	//fields
+	private LocalDateTime departureDateTime;
+	
+	//constructor
+	public Charter(int flightNumber, String departureCity, String arrivalCity, String departureDateTime) {
+		
+		super(flightNumber, departureCity, arrivalCity);
+		setDepartureDateTime(departureDateTime);
+	}
+	
+
+	public LocalDateTime getDepartureDateTime() {
+		return departureDateTime;
+	}
+
+
+	public void setDepartureDateTime(String departureDateTime) {
+		this.departureDateTime =  LocalDateTime.parse(departureDateTime);
+	}
+	
+	
+	
+
+}
